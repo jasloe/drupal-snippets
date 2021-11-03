df0befe562f24616f2d4b5941ee2df72a7ab62ca
Jason Loeffler <j@minorscience.com>
2021-11-03 19:07:59 -0400

    Update set_kint_depth.php

--- set_kint_depth.php
+++ set_kint_depth.php
@@ -3,3 +3,15 @@ if (class_exists('Kint')) {
   // Set the max_depth to prevent out-of-memory.
   \Kint::$max_depth = 4;
 }
+
+// Enable ClassMethodsPlugin, ClassStaticsPlugin, and IteratorPlugin
+if (class_exists('Kint')) {
+  // Set the max_depth to prevent out-of-memory.
+  \Kint::$max_depth = 4;
+  // Set Kint plugins
+  \Kint::$plugins = array_merge(\Kint::$plugins, [
+  	'\\Kint\\Parser\\ClassMethodsPlugin',
+  	'\\Kint\\Parser\\ClassStaticsPlugin',
+  	'\\Kint\\Parser\\IteratorPlugin',
+  ]);
+}
